20d92f35f36d5701e6fcfa6efa3ed10dec0876e3
Saborknight <anton@brink.es>
2017-03-09 00:20:36 +0000

    Feature - Created "Projects" route for use
    What
    =================
    Set up a "Projects" route for the projects page which will be displaying
    my past projects.
    
    Why
    =================
    
    Ticket: N/A

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,11 +83,8 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://facebook.com/Saborknight">Facebook</a>
+                    <a href="https://github.com/Saborknight/portfolio">GitHub</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/projects', function() {
+	return view('projects');
+});
